b7d2a7e3b387949ca1626aaffe0f9b73eefefa20
Cameron <e107inc@gmail.com>
2016-05-23 12:49:13 -0700

    Global language file added.

---
+++ languages/English/English_global.php
@@ -0,0 +1,11 @@
+<?php
+	/**
+	 * e107 website system
+	 *
+	 * Copyright (C) 2008-2016 e107 Inc (e107.org)
+	 * Released under the terms and conditions of the
+	 * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+	 *
+	 */
+
+define("LAN_PLUGIN_VSTORE_NAME", "Shop");
