f8405d0491bcfe70a7d7895db8880e617eed2d08
Gábor <yesszus@gmail.com>
2016-11-13 08:59:27 +0100

    Create Hungarian_global.php

---
+++ languages/Hungarian/Hungarian_global.php
@@ -0,0 +1,13 @@
+<?php
+	/**
+	 * e107 website system
+	 *
+	 * Copyright (C) 2008-2016 e107 Inc (e107.org)
+	 * Released under the terms and conditions of the
+	 * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+	 *
+	 */
+
+define("LAN_PLUGIN_VSTORE_NAME", "Shop");
+define("LAN_PLUGIN_VSTORE_SUMMARY", "Egyszerű Virtuális Áruház e107 weboldalakra.");
+define("LAN_PLUGIN_VSTORE_DESCRIPTION", "Bevásárló rendszer e107 v2 weboldalakra.");
