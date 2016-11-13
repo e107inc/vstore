97193226495fc1fd832db8a0bbe834ef0fb126bf
Gábor <yesszus@gmail.com>
2016-11-13 11:02:16 +0100

    Add files via upload

---
+++ languages/Hungarian/Hungarian_admin.php
@@ -0,0 +1,63 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/11/05 10:20:43
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
+
+define("LAN_VSTORE_ADMIN_01", "Termékek");
+define("LAN_VSTORE_ADMIN_02", "Termék hozzáadása");
+define("LAN_VSTORE_ADMIN_03", "Eladások");
+define("LAN_VSTORE_ADMIN_04", "Fizetési Átjárók");
+define("LAN_VSTORE_ADMIN_05", "Családnév");
+define("LAN_VSTORE_ADMIN_06", "Keresztnév");
+define("LAN_VSTORE_ADMIN_07", "Admin terület");
+define("LAN_VSTORE_ADMIN_08", "Szülő");
+define("LAN_VSTORE_ADMIN_09", "Alap");
+define("LAN_VSTORE_ADMIN_010", "Áttekintés");
+define("LAN_VSTORE_ADMIN_011", "Képek/Videók");
+define("LAN_VSTORE_ADMIN_012", "Szállítási költség");
+define("LAN_VSTORE_ADMIN_013", "Session");
+define("LAN_VSTORE_ADMIN_014", "Kapcsolódó");
+define("LAN_VSTORE_ADMIN_015", "Raktárkészlet");
+define("LAN_VSTORE_ADMIN_016", "Külső Link");
+define("LAN_VSTORE_ADMIN_017", "Fájl Letöltés");
+define("LAN_VSTORE_ADMIN_018", "Kiszállítva");
+define("LAN_VSTORE_ADMIN_019", "Elemek");
+define("LAN_VSTORE_ADMIN_020", "Átjáró");
+define("LAN_VSTORE_ADMIN_021", "Fizetési állapot");
+define("LAN_VSTORE_ADMIN_022", "Nyers adatok");
+define("LAN_VSTORE_ADMIN_023", "Ismertetés");
+define("LAN_VSTORE_ADMIN_024", "Fül Neve:");
+define("LAN_VSTORE_ADMIN_025", "Forrás:");
+define("LAN_VSTORE_ADMIN_026", "Előnév");
+define("LAN_VSTORE_ADMIN_027", "Nappal");
+define("LAN_VSTORE_ADMIN_028", "Éjjel");
+define("LAN_VSTORE_ADMIN_029", "Címzett");
+define("LAN_VSTORE_ADMIN_030", "Érdekelt");
+define("LAN_VSTORE_ADMIN_031", "Megjegyzés");
+define("LAN_VSTORE_ADMIN_032", "Kód");
+define("LAN_VSTORE_ADMIN_033", "Store Caption");
+define("LAN_VSTORE_ADMIN_034", "Category Caption");
+define("LAN_VSTORE_ADMIN_035", "Out-of-Stock Caption");
+define("LAN_VSTORE_ADMIN_036", "Pénznem");
+define("LAN_VSTORE_ADMIN_037", "Pénznem kiválasztása");
+define("LAN_VSTORE_ADMIN_038", "Kiszállítás bekalkulálása");
+define("LAN_VSTORE_ADMIN_039", "Including shipping calculation at checkout.");
+define("LAN_VSTORE_ADMIN_040", "Hogyan rendeljünk info megadása.");
+define("LAN_VSTORE_ADMIN_041", "Termékek per oldal");
+define("LAN_VSTORE_ADMIN_042", "Kategóriák per oldal");
+define("LAN_VSTORE_ADMIN_043", "Paypal Fizetések");
+define("LAN_VSTORE_ADMIN_044", "Paypal Felhasználónév");
+define("LAN_VSTORE_ADMIN_045", "Paypal Jelszó");
+define("LAN_VSTORE_ADMIN_046", "Paypal Aláírás");
+define("LAN_VSTORE_ADMIN_047", "Amazon Fizetések");
+define("LAN_VSTORE_ADMIN_048", "Amazon Régió");
+define("LAN_VSTORE_ADMIN_049", "Skrill Fizetések");
+
+?>
