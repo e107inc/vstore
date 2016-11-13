447461c25e1559a6b454d397db1edc515bdaf358
Gábor <yesszus@gmail.com>
2016-11-13 08:59:48 +0100

    Add files via upload

---
+++ languages/Hungarian/Hungarian_front.php
@@ -0,0 +1,63 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/11/05 10:20:43
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
+
+define("LAN_VSTORE_001", "Termékek");
+define("LAN_VSTORE_002", "Nincs raktáron");
+define("LAN_VSTORE_003", "Kosárba rakom");
+define("LAN_VSTORE_004", "Raktáron");
+define("LAN_VSTORE_005", "Kosár");
+define("LAN_VSTORE_006", "Fizetés");
+define("LAN_VSTORE_007", "Az Ön kosara üres.");
+define("LAN_VSTORE_008", "Vásárlás folytatása");
+define("LAN_VSTORE_009", "A fizetési opció még nincs beállítva!");
+define("LAN_VSTORE_010", "Price may change due to exchange rate.");
+define("LAN_VSTORE_011", "Termék Kód");
+define("LAN_VSTORE_012", "Készlet");
+define("LAN_VSTORE_013", "A kategóriában nincs elérhető termék!");
+define("LAN_VSTORE_014", "Re-Calculate");
+define("LAN_VSTORE_015", "Új");
+define("LAN_VSTORE_016", "Processing");
+define("LAN_VSTORE_017", "On Hold");
+define("LAN_VSTORE_018", "Completed");
+define("LAN_VSTORE_019", "Visszamondva");
+define("LAN_VSTORE_020", "Refunded");
+define("LAN_VSTORE_021", "Shipping Details");
+define("LAN_VSTORE_022", "First Name");
+define("LAN_VSTORE_023", "Last Name");
+define("LAN_VSTORE_024", "Confirm Password");
+define("LAN_VSTORE_025", "Társaság");
+define("LAN_VSTORE_026", "Lakcím");
+define("LAN_VSTORE_027", "Város");
+define("LAN_VSTORE_028", "Megye");
+define("LAN_VSTORE_029", "Irányítószám");
+define("LAN_VSTORE_030", "Ország");
+define("LAN_VSTORE_031", "Ország kiválasztása...");
+define("LAN_VSTORE_032", "Telefonszám");
+define("LAN_VSTORE_033", "Order Notes");
+define("LAN_VSTORE_034", "Special notes for delivery.");
+define("LAN_VSTORE_035", "Select payment method to continue");
+define("LAN_VSTORE_036", "Invalid Payment Type");
+define("LAN_VSTORE_037", "Shopping Cart Empty");
+define("LAN_VSTORE_038", "Unable to save transaction");
+define("LAN_VSTORE_039", "Hogyan rendeljünk");
+define("LAN_VSTORE_040", "Mennyiség");
+define("LAN_VSTORE_041", "Ár");
+define("LAN_VSTORE_042", "Fizetendő");
+define("LAN_VSTORE_043", "Termék név");
+define("LAN_VSTORE_044", "Részösszeg");
+define("LAN_VSTORE_045", "Kiszállítás díja");
+define("LAN_VSTORE_046", ""); //üres
+define("LAN_VSTORE_047", "Az Ön #[x] rendelése kész!");
+define("LAN_VSTORE_048", "Részletek"); 
+define("LAN_VSTORE_049", "Állapot"); 
+
+?>
