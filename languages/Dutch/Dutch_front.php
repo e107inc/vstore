01ba087ce7e96d3cdfab43fc20eef9d8a4522dc9
leo <the_guardian2@hotmail.com>
2021-03-05 16:40:20 +0100

    Language Support
    Language Support

---
+++ languages/Dutch/Dutch_front.php
@@ -0,0 +1,45 @@
+<?php
+	/**
+	 * e107 website system
+	 *
+	 * Copyright (C) 2008-2021 e107 Inc (e107.org)
+	 * Released under the terms and conditions of the
+	 * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+	 *
+	 */
+
+define("LAN_VSTORE_001", "In winkelwagen");
+define("LAN_VSTORE_002", "Op voorraad");
+define("LAN_VSTORE_003", "Niet op voorraad");
+
+
+define("LAN_VSTORE_004", "Vstore Categorieën");
+define("LAN_VSTORE_005", "Wilt u deze  order echt annuleren");
+define("LAN_VSTORE_006", "Nee, ga terug");
+define("LAN_VSTORE_007", "Ja, annuleer deze bestelling!");
+//define("LAN_VSTORE_008", "Her-bereken");
+define("LAN_VSTORE_009", "Coupon-code niet juist!");
+define("LAN_VSTORE_010", "Sorry, maar wij kunnen het geselecteerde item niet vinden");
+define("LAN_VSTORE_011", "of het is niet langer meer beschikbaar!"); 
+define("LAN_VSTORE_012", "Ik ben een nieuwe klant");
+define("LAN_VSTORE_013", "Door u in te schrijven op onze website. kunt u sneller aankopen doen, 
+			altijd op de hoogte zijn van de status van uw bestellingen en u heeft 
+			altijd een up-to-date overzicht over uw bestellingen.");
+define("LAN_VSTORE_014", "Bestel als Gast");
+define("LAN_VSTORE_015", "Wanneer u als gast besteld,wordt er geen gebruikers account aangemaakt.<br/>
+			Dat betekent, bij een nieuwe bestelling, u opnieuw alle
+			informatie moet invullen.");
+define("LAN_VSTORE_016", "Ik heb reeds een gebruikers account");
+define("LAN_VSTORE_017", "Laat mij inloggen...");
+define("LAN_VSTORE_018", "Inschrijven");
+define("LAN_VSTORE_019", "Inloggen");
+//define("LAN_VSTORE_020", "Uw bestelling ");
+//define("LAN_VSTORE_021", "is compleet en u zult binnen enkele minuten ");
+//define("LAN_VSTORE_022", "via email een orderbevestiging ontvangen, met daarin alle details !");
+
+define("LAN_VSTORE_023", "Je moet");  //part error mess
+define("LAN_VSTORE_024", "om deze pagina te kunnen bekijken!"); //part error mess
+define("LAN_VSTORE_025", "Het ingevoerde aantal voor"); 
+define("LAN_VSTORE_026", "is hoger dan het aantal op voorraad!<br/>Het aantal werd aangepast!"); 
+define("LAN_VSTORE_027", "Geen produkten beschikbaar in deze categorie"); 
+define("LAN_VSTORE_028", "Het spijt ons, maar dit item is niet meer beschikbaar!"); 
---
+++ languages/French/French_global.php
@@ -0,0 +1,281 @@
+<?php
+	/**
+	 * Système de site web e107
+	 *
+	 * Copyright (C) 2008-2021 e107 Inc (e107.org)
+	 * Publié selon les termes et conditions de la
+	 * Licence publique générale GNU (http://www.gnu.org/licenses/gpl.txt)
+	 *
+	 */
+
+define("LAN_PLUGIN_VSTORE_NAME", "Shop");
+define("LAN_PLUGIN_VSTORE_PRICE", "Prix");
+define("LAN_VSTORE_DSTAT_001", "Commandes ouvertes") ;
+define("LAN_VSTORE_DSTAT_002", "Aucune transaction trouvée") ;
+define("LAN_VSTORE_DSTAT_003", "Recettes mensuelles : ") ;
+define("LAN_VSTORE_DSTAT_004", "Commandes de ce mois") ;
+define("LAN_VSTORE_DSTAT_005", "et") ;
+
+//define("LAN_VSTORE_PREF_001", "Comment commander") ;
+//define("LAN_VSTORE_008", "Recalculer") ;
+//define("LAN_VSTORE_STAT_005", "Commandes payées et ouvertes") ;
+//define("LAN_VSTORE_STAT_DATA", "Aucune commande disponible pour la dernière semaine.");
+
+
+define("LAN_VSTORE_GEN_001", "Statut");
+define("LAN_VSTORE_GEN_002", "Articles");
+define("LAN_VSTORE_GEN_003", "Qté");
+define("LAN_VSTORE_GEN_004", "Log");
+define("LAN_VSTORE_GEN_005", "Adresse de facturation");
+define("LAN_VSTORE_GEN_006", "Date");
+define("LAN_VSTORE_GEN_007", "Ordre");
+define("LAN_VSTORE_GEN_008", "Facture");
+define("LAN_VSTORE_GEN_009", "Mode de paiement");
+define("LAN_VSTORE_GEN_010", "Paiement complet");
+define("LAN_VSTORE_GEN_011", "Informations sur la commande");
+define("LAN_VSTORE_GEN_012", "Expédition");
+define("LAN_VSTORE_GEN_013", "Total");
+define("LAN_VSTORE_GEN_014", "Produits");             
+define("LAN_VSTORE_GEN_015", "Adresse de facturation");
+define("LAN_VSTORE_GEN_016", "Actions");     
+define("LAN_VSTORE_GEN_017", "Facturation");
+define("LAN_VSTORE_GEN_018", "Nouveau");
+define("LAN_VSTORE_GEN_019", "Détails de la commande");
+define("LAN_VSTORE_GEN_020", "Détails d'expédition");
+define("LAN_VSTORE_GEN_021", "requis");
+define("LAN_VSTORE_GEN_022", "Téléphone");
+define("LAN_VSTORE_GEN_023", "Fax");
+define("LAN_VSTORE_GEN_024", "Taxe");
+define("LAN_VSTORE_GEN_025", "Traitement");
+define("LAN_VSTORE_GEN_026", "Annulé");
+define("LAN_VSTORE_GEN_027", "En attente");
+define("LAN_VSTORE_GEN_028", "Terminé");
+define("LAN_VSTORE_GEN_029", "Remboursé");
+define("LAN_VSTORE_GEN_030", "Comment commander");
+define("LAN_VSTORE_GEN_031", "Re-order");
+define("LAN_VSTORE_GEN_032", "Commandes payées et ouvertes");
+define("LAN_VSTORE_GEN_033", "Fichiers") ;
+define("LAN_VSTORE_GEN_034", "Pas de commandes disponibles depuis une semaine !");
+
+define("LAN_VSTORE_CART_001", "Code produit");
+define("LAN_VSTORE_CART_002", "Disponibilité");
+define("LAN_VSTORE_CART_003", "Le prix peut varier en raison du taux de change");
+define("LAN_VSTORE_CART_004", "Le domaine");
+define("LAN_VSTORE_CART_005", "Produit");
+//define("LAN_VSTORE_CART_006", "Taxe");
+define("LAN_VSTORE_CART_007", "est nécessaire !");
+define("LAN_VSTORE_CART_008", "Facturation");
+define("LAN_VSTORE_CART_009", "Sous-total");
+define("LAN_VSTORE_CART_010", "Quantité");
+define("LAN_VSTORE_CART_011", "Estimation de l'expédition");
+define("LAN_VSTORE_CART_012", "Entrez le code du coupon si disponible");
+define("LAN_VSTORE_CART_013", "Continuer les achats");
+define("LAN_VSTORE_CART_014", "Commander");
+define("LAN_VSTORE_CART_015", "par");
+define("LAN_VSTORE_CART_016", "Effacer le panier");
+define("LAN_VSTORE_CART_017", "Mon tableau de bord");
+define("LAN_VSTORE_CART_018", "Tableau de bord");
+define("LAN_VSTORE_CART_019", "Mes commandes");
+define("LAN_VSTORE_CART_020", "Mes téléchargements");
+define("LAN_VSTORE_CART_021", "Mes adresses");
+
+define("LAN_VSTORE_CART_022", "Mon compte");
+define("LAN_VSTORE_CART_023", "Afficher les détails");
+define("LAN_VSTORE_CART_024", "Annuler la commande");
+define("LAN_VSTORE_CART_025", "Modifier l'adresse");
+define("LAN_VSTORE_CART_026", "Supprimer");
+define("LAN_VSTORE_CART_027", "Depuis le tableau de bord de votre compte, vous pouvez consulter vos commandes récentes, gérer vos adresses de livraison et de facturation et modifier votre mot de passe et les détails de votre compte");
+define("LAN_VSTORE_CART_029", "Votre panier est vide");
+define("LAN_VSTORE_CART_030", "Commencez vos achats");
+define("LAN_VSTORE_CART_031", "Entrez l'adresse de livraison");
+define("LAN_VSTORE_CART_032", "Utilisez ce bouton pour utiliser ou entrer une adresse de livraison séparée");
+define("LAN_VSTORE_CART_033", "Sélectionner le mode de paiement pour continuer");
+define("LAN_VSTORE_CART_034", "Aucune commande n'a encore été effectuée.");
+define("LAN_VSTORE_CART_035", "Aucun fichier téléchargeable pour l'instant.");
+define("LAN_VSTORE_CART_036", "Aucune adresse disponible pour le moment.");
+define("LAN_VSTORE_CART_037", "Votre panier est vide.");
+define("LAN_VSTORE_CART_038", "Notes");
+define("LAN_VSTORE_CART_039", "Aucune adresse de livraison définie !");
+define("LAN_VSTORE_CART_040", "Utilisez les informations bancaires suivantes pour votre paiement :");
+define("LAN_VSTORE_CART_041", "Le coupon n'est plus disponible !<br />Il a dépassé le nombre d'utilisation autorisé !");
+define("LAN_VSTORE_CART_042", "Le coupon n'est plus disponible !");
+define("LAN_VSTORE_CART_043", "Le coupon n'est pas encore disponible !");
+define("LAN_VSTORE_CART_044", "Le coupon n'est pas disponible !");
+define("LAN_VSTORE_CART_045", "No. de facture :");
+define("LAN_VSTORE_CART_046", "N° de commande : ");
+define("LAN_VSTORE_CART_047", "Date de commande :");
+//define("LAN_VSTORE_CART_048", "");
+define("LAN_VSTORE_CART_049", "Date limite de paiement :");
+
+define("LAN_VSTORE_CART_051", "La quantité du produit sélectionné dépasse le nombre d'articles en stock !");
+define("LAN_VSTORE_CART_052", "La quantité a été ajustée !");
+
+// portail pour une utilisation mondiale
+define("LAN_VSTORE_GATE_003", "Virement bancaire");
+define("LAN_VSTORE_GATE_004", "Coordonnées bancaires");
+define("LAN_VSTORE_GATE_005", "Zone de texte pour les détails du compte bancaire"); //haut de la page d'accueil
+
+// Facturation à la sortie du client - Facturation à l'avance et utilisation interne
+define("LAN_VSTORE_CUSM_001", "Prénom");
+define("LAN_VSTORE_CUSM_002", "Nom");
+define("LAN_VSTORE_CUSM_003", "Société");
+define("LAN_VSTORE_CUSM_004", "Numéro de TVA");
+define("LAN_VSTORE_CUSM_005", "Code fiscal");
+define("LAN_VSTORE_CUSM_006", "Adresse");
+define("LAN_VSTORE_CUSM_007", "Ville");
+define("LAN_VSTORE_CUSM_008", "État/Région"); 
+define("LAN_VSTORE_CUSM_009", "Zip/Code postal");
+define("LAN_VSTORE_CUSM_010", "Pays");
+define("LAN_VSTORE_CUSM_011", "Select Country");
+define("LAN_VSTORE_CUSM_012", "Adresse électronique");
+define("LAN_VSTORE_CUSM_013", "Champ obligatoire"); //textuel
+define("LAN_VSTORE_CUSM_014", "Numéro de téléphone");
+define("LAN_VSTORE_CUSM_015", "Numéro de fax");
+//define("LAN_VSTORE_CUSM_016", "");
+define("LAN_VSTORE_CUSM_017", "Adresse de facturation manquante !");
+define("LAN_VSTORE_CUSM_018", "Adresse de livraison");
+
+
+//checkout sales
+define("LAN_VSTORE_CUSM_019", "Résumé");
+define("LAN_VSTORE_CUSM_020", "Adresse de facturation et de livraison");
+define("LAN_VSTORE_CUSM_021", "Mode de paiement sélectionné");
+define("LAN_VSTORE_CUSM_022", "Description");
+define("LAN_VSTORE_CUSM_023", "Prix unitaire");
+define("LAN_VSTORE_CUSM_024", "Qté");
+define("LAN_VSTORE_CUSM_025", "Montant");
+define("LAN_VSTORE_CUSM_026", "Expédition");
+define("LAN_VSTORE_CUSM_027", "TVA incluse");
+define("LAN_VSTORE_CUSM_028", "et payé");
+define("LAN_VSTORE_CUSM_029", "Bons de commande");
+define("LAN_VSTORE_CUSM_030", "Bons de livraison spéciaux");
+define("LAN_VSTORE_CUSM_031", "Achetez maintenant !");
+define("LAN_VSTORE_CUSM_032", "Rupture de stock");
+define("LAN_VSTORE_CUSM_033", "Payé");
+define("LAN_VSTORE_CUSM_034", "Ouvert");
+define("LAN_VSTORE_CUSM_036", "Download");
+define("LAN_VSTORE_CUSM_037", "Télécharger (sera disponible une fois le paiement reçu)");
+define("LAN_VSTORE_CUSM_038", "Commande créée");
+define("LAN_VSTORE_CUSM_039", "Coupon : ");
+define("LAN_VSTORE_CUSM_040", "Détails de l'expédition");
+define("LAN_VSTORE_CUSM_041", "Vstore Open/Payed");
+define("LAN_VSTORE_CUSM_042", "Commandes ouvertes");
+define("LAN_VSTORE_CUSM_043", "Date/Heure");
+define("LAN_VSTORE_CUSM_044", "Numéro de facture attribué :");
+define("LAN_VSTORE_CUSM_045", "N° de réf. de commande : attribué");
+define("LAN_VSTORE_CUSM_046", "Impossible de charger la commande !");
+define("LAN_VSTORE_CUSM_047", "Ordre non trouvé !");
+define("LAN_VSTORE_CUSM_048", "Impossible d'insérer/mettre à jour la commande !");
+define("LAN_VSTORE_CUSM_049", "Impossible de changer le statut de la commande. Aucune commande chargée !");
+define("LAN_VSTORE_CUSM_050", "Impossible de mettre à jour le statut de la commande.");
+define("LAN_VSTORE_CUSM_051", "Changement de [x] de [y] à [z].");
+define("LAN_VSTORE_CUSM_052", "Pas de commande chargée. Impossible de mettre à jour la référence de la commande !");
+define("LAN_VSTORE_CUSM_053", "La commande est déjà remboursée !");
+define("LAN_VSTORE_CUSM_054", "Seules les commandes avec le statut 'En cours de traitement', 'En attente' et 'Terminé' peuvent être remboursées !");
+define("LAN_VSTORE_CUSM_055", "Passerelle (gateway) manquante !");
+define("LAN_VSTORE_CUSM_056", "Détails de remboursement non définis !");
+define("LAN_VSTORE_CUSM_057", "Remboursé");
+define("LAN_VSTORE_CUSM_058", "Commande remboursée le [x] par [y] ([z])");
+define("LAN_VSTORE_CUSM_059", "La commande a été marquée comme remboursée, mais le mode de paiement");
+define("LAN_VSTORE_CUSM_060", "ne permet pas le remboursement automatique, vous devez le faire manuellement");
+define("LAN_VSTORE_CUSM_061", "Non.");
+define("LAN_VSTORE_CUSM_062", "Prix");
+define("LAN_VSTORE_CUSM_063", "Aucun mode de paiement sélectionné !");
+define("LAN_VSTORE_CUSM_064", "Aucun article à commander !");
+define("LAN_VSTORE_CUSM_065", "Pas d'ensemble de données client !");
+define("LAN_VSTORE_CUSM_066", "Pas d'enregistrement envoyé !<br />Veuillez indiquer l'adresse de livraison");
+define("LAN_VSTORE_CUSM_067", "Commande annulée par l'utilisateur");
+define("LAN_VSTORE_CUSM_068", "Changements enregistrés avec succès !");
+define("LAN_VSTORE_CUSM_069", "Fichier téléchargé avec succès !");
+define("LAN_VSTORE_CUSM_070", "L'adresse de facturation est manquante !");
+define("LAN_VSTORE_CUSM_071", "Aucun article à commander !");
+define("LAN_VSTORE_CUSM_072", "Quelque chose a mal tourné ! Impossible de sauvegarder les modifications !");
+define("LAN_VSTORE_CUSM_073", "Pas d'adresse de facturation définie");
+define("LAN_VSTORE_CUSM_074", "Pas d'options de paiement définies");
+define("LAN_VSTORE_CUSM_075", "Type de paiement non valable");
+define("LAN_VSTORE_CUSM_076", "Impossible de sauvegarder la transaction");
+define("LAN_VSTORE_CUSM_077", "Article non trouvé !");
+define("LAN_VSTORE_CUSM_078", "Facture pdf non trouvée !");
+define("LAN_VSTORE_CUSM_079", "Aucune catégorie disponible !");
+define("LAN_VSTORE_CUSM_080", "Le remboursement a échoué !");
+define("LAN_VSTORE_CUSM_081", "Ordre non chargé !");
+define("LAN_VSTORE_CUSM_082", "Nombre d'élément_vars invalide!");
+define("LAN_VSTORE_CUSM_083", "Facture pdf introuvable!");
+
+define("LAN_VSTORE_CUST_013", "Commande mise à jour vers");
+define("LAN_VSTORE_CUST_014", "Ordre ne peut pas être mis à jour en ");
+define("LAN_VSTORE_020", "Votre commande");
+define("LAN_VSTORE_021", "est complet et vous recevrez dans quelques minutes");
+define("LAN_VSTORE_022", "recevra une confirmation de commande par e-mail avec tous les détails !");
+//email admin
+define("LAN_VSTORE_MAIL_001", "Réinitialiser le modèle");
+define("LAN_VSTORE_MAIL_002", "Cliquer et enregistrer pour réinitialiser ce modèle au modèle par défaut");
+define("LAN_VSTORE_MAIL_003", "Recevoir un courriel en CC ?");
+define("LAN_VSTORE_MAIL_004", "Utilisation des options de champ");
+define("LAN_VSTORE_MAIL_005", "Le numéro de référence de la commande");
+define("LAN_VSTORE_MAIL_006", "Le prénom de facturation");
+define("LAN_VSTORE_MAIL_007", "Le nom de famille de la facturation");
+define("LAN_VSTORE_MAIL_008", "Nom de l'entreprise de facturation");
+define("LAN_VSTORE_MAIL_009", "La rue de facturation");
+define("LAN_VSTORE_MAIL_010", "La ville de facturation");
+define("LAN_VSTORE_MAIL_011", "L'état de facturation");
+define("LAN_VSTORE_MAIL_012", "Le code postal de facturation");
+define("LAN_VSTORE_MAIL_013", "Le pays de facturation");
+define("LAN_VSTORE_MAIL_014", "Le prénom de l'expéditeur");
+define("LAN_VSTORE_MAIL_015", "Le nom de famille de l'expéditeur");
+define("LAN_VSTORE_MAIL_016", "Nom de la compagnie maritime");
+define("LAN_VSTORE_MAIL_017", "La rue de navigation");
+define("LAN_VSTORE_MAIL_018", "The shipping city");
+define("LAN_VSTORE_MAIL_019", "L'État de navigation");
+define("LAN_VSTORE_MAIL_020", "Le code postal d'expédition");
+define("LAN_VSTORE_MAIL_021", "Le pays d'expédition");
+define("LAN_VSTORE_MAIL_022", "Les articles commandés");
+define("LAN_VSTORE_MAIL_023", "En cas de mode de paiement 'virement bancaire', les détails du virement bancaire");
+define("LAN_VSTORE_MAIL_024", "Nom et adresse du commerçant");
+define("LAN_VSTORE_MAIL_025", "Le nom de l'expéditeur est défini dans les préférences du magasin");
+define("LAN_VSTORE_MAIL_026", "Confirmation de commande");
+define("LAN_VSTORE_MAIL_027", "Commande terminée");
+define("LAN_VSTORE_MAIL_028", "Commande annulée");
+define("LAN_VSTORE_MAIL_029", "Commande remboursée");
+define("LAN_VSTORE_MAIL_030", "Date de la commande");
+define("LAN_VSTORE_MAIL_031", "et payé");
+define("LAN_VSTORE_MAIL_032", "Modèle de facture 'par défaut' introuvable !");
+
+define("LAN_VSTORE_CUST_001", "Dernière mise à jour");
+define("LAN_VSTORE_CUST_002", "Titre");
+define("LAN_VSTORE_CUST_003", "Marchand");
+define("LAN_VSTORE_CUST_004", "Client");
+
+define("LAN_VSTORE_CUST_009", "Ville");
+define("LAN_VSTORE_CUST_010", "État");
+//define("LAN_VSTORE_CUST_011", "");
+define("LAN_VSTORE_CUST_012", "Pays");
+//define("LAN_VSTORE_CUST_013", "Commande mise à jour vers");
+//define("LAN_VSTORE_CUST_014", "Ordre ne peut pas être mis à jour en ");
+define("LAN_VSTORE_CUST_015", "Utiliser l'adresse de livraison");
+define("LAN_VSTORE_CUST_016", "Dernière adresse de livraison");
+define("LAN_VSTORE_CUST_017", "Champs supplémentaires");
+define("LAN_VSTORE_CUST_018", "Notes internes");
+define("LAN_VSTORE_CUST_019", "Refcode");
+define("LAN_VSTORE_FWEI_001", "Poids:");
+
+ //mess
+define("LAN_VSTORE_ERR_001", "Votre commande est toujours en état");
+define("LAN_VSTORE_ERR_002", "qui ne permet pas de télécharger le fichier !");
+define("LAN_VSTORE_ERR_003", "Nous n'avons pas pu trouver votre commande et le téléchargement a donc été refusé !");
+define("LAN_VSTORE_ERR_004", "Vous devez vous connecter pour télécharger le fichier !");
+define("LAN_VSTORE_ERR_005", "Identifiant de téléchargement");
+define("LAN_VSTORE_ERR_006", "manquants ou invalides !");
+define("LAN_VSTORE_ERR_007", "ne contient pas de fichier à télécharger !");
+define("LAN_VSTORE_ERR_008", "à télécharger manquant ou invalide !");
+define("LAN_VSTORE_ERR_009", "Vous avez annulé votre paiement, mais votre panier est toujours disponible pour consultation ultérieure.");
+define("LAN_VSTORE_ERR_010", "Votre paiement a échoué pour une raison quelconque, mais votre panier est toujours disponible pour consultation.");
+define("LAN_VSTORE_ERR_011", "Le coupon dépasse le nombre d'utilisation autorisé pour cet article");
+define("LAN_VSTORE_ERR_012", "L'adresse e-mail donnée n'est pas valide !");
+define("LAN_VSTORE_ERR_013", "Certaines données ont déjà été envoyées au navigateur, ne peuvent pas envoyer de fichier PDF");
+define("LAN_VSTORE_ERR_014", "Adresse invalide !");
+
+define("LAN_VSTORE_WARN_011", "Nous sommes désolés, mais l'article");
+define("LAN_VSTORE_WARN_012", "est manquant ou n'est plus actif et a été retiré du panier !");
+define("LAN_VSTORE_WARN_013", "La quantité de l'article dépasse le nombre d'utilisation du code de réduction autorisé pour cet article");
+define("LAN_VSTORE_WARN_014", "Le coupon ne sera utilisé que pour le nombre d'utilisations restant");
